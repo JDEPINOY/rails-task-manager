958ca3f0f385d97c50cd8009bf4c8eb160810f3b
jeoffrey depinoy <jdepinoy@norauto.com>
2018-05-22 17:25:45 +0200

    optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  post "tasks", to: "tasks#create"
-  get 'tasks/new', to: "tasks#new", as: :new
-  get 'tasks/:id', to: 'tasks#show'
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update", as: :task
-  delete "tasks/:id", to: "tasks#destroy"
+  # get 'tasks', to: 'tasks#index'
+  #root to: 'tasks#index'
+  #post "tasks", to: "tasks#create"
+  # get 'tasks/new', to: "tasks#new", as: :new
+  # get 'tasks/:id', to: 'tasks#show'
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  #patch "tasks/:id", to: "tasks#update", as: :task
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks #, only: [:index, :destroy, :show, :edit, :new, :tasks, :task]
 end
